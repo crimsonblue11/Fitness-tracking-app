9d7cb05f99b6b02db2884504f4d4da5e21618973
psycd8 <psycd8@exmail.nottingham.ac.uk>
2024-01-12 12:19:18 +0000

    document and refactor map package

--- app/src/main/java/com/example/mdp_cw2/map/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/example/mdp_cw2/map/GeofenceBroadcastReceiver.java
@@ -1,16 +1,17 @@
+/**
+ * Broadcast receiver to check for Geofence status changes.
+ * Used to send notifications for LBRs.
+ */
+
 package com.example.mdp_cw2.map;
 
 import android.Manifest;
 import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
@@ -21,6 +22,9 @@ import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
 
 public class GeofenceBroadcastReceiver extends BroadcastReceiver {
+    /**
+     * Static constant representing ID of the notification that will be sent.
+     */
     private static final int NOTIFICATION_ID = 2;
 
     @Override
@@ -32,31 +36,28 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
             return;
         }
 
+        // get LBR title from the intent
         String reminderText = intent.getStringExtra("lbrName");
 
+        // get the specific type of transition from the event
         int transition = event.getGeofenceTransition();
 
         if (transition == Geofence.GEOFENCE_TRANSITION_ENTER) {
-            // TODO - show notification
+            // if entering geofence, show a notification
             Notification notif = new NotificationCompat.Builder(context, MainActivity.REMINDER_CHANNEL_ID)
                     .setContentTitle("Reminder: " + reminderText)
                     .setContentText("This is a location-based reminder")
                     .build();
 
             if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+                // don't do anything
                 return;
             }
             NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, notif);
 
         } else if(transition == Geofence.GEOFENCE_TRANSITION_EXIT) {
-            // TODO - remove notification
+            // remove notification
+            NotificationManagerCompat.from(context).cancel(NOTIFICATION_ID);
         }
     }
 
--- app/src/main/java/com/example/mdp_cw2/map/MapsFragment.java
+++ app/src/main/java/com/example/mdp_cw2/map/MapsFragment.java
@@ -1,3 +1,8 @@
+/**
+ * Fragment containing Google Map component.
+ * Also used for managing and viewing LBRs.
+ */
+
 package com.example.mdp_cw2.map;
 
 import androidx.annotation.NonNull;
@@ -48,15 +53,45 @@ import java.util.List;
 import java.util.UUID;
 
 public class MapsFragment extends AppFragment {
+    /**
+     * Reference to parent activity.
+     */
     private MainActivity activity;
+
+    /**
+     * Geofencing client instance.
+     */
     private GeofencingClient geofencingClient;
+
+    /**
+     * DAO object.
+     */
     private LogDao dao;
+
+    /**
+     * Button to add an LBR after adding a circle to the map.
+     */
     private Button addLBRButton;
+
+    /**
+     * Button to cancel adding an LBR after adding a circle to the map.
+     */
     private Button cancelButton;
+
+    /**
+     * Edit text for the title of the reminder to add.
+     */
     private EditText reminderEditText;
+
+    /**
+     * List of all LBRs to display on the map.
+     */
     final ArrayList<LBRItem> LBRs = new ArrayList<>();
+
+    /**
+     * Reference to the current LBR that is being added, if adding an LBR (null if otherwise).
+     */
     private Circle newLBR;
-    private Button clearLBRs;
 
     private final OnMapReadyCallback callback = new OnMapReadyCallback() {
         /**
@@ -66,12 +101,12 @@ public class MapsFragment extends AppFragment {
          */
         @Override
         public void onMapReady(@NonNull GoogleMap googleMap) {
-            MainActivity activity = (MainActivity) getActivity();
             activity.getViewModel().getLBRs().observe(activity, lbrItems -> {
                 LBRs.clear();
                 LBRs.addAll(lbrItems);
 
                 for (LBRItem lbr : LBRs) {
+                    // convert all LBRs into circles on the map
                     googleMap.addCircle(new CircleOptions()
                             .center(new LatLng(lbr.latitude, lbr.longitude))
                             .radius(lbr.radius)
@@ -83,10 +118,15 @@ public class MapsFragment extends AppFragment {
                 }
 
                 googleMap.setOnMapClickListener(l -> {
-                    // TODO - better option?
+                    // upon clicking the map, a new circle is added
+                    // if this is submitted it will become a new LBR and Geofence
+
                     if (newLBR != null) {
+                        // remove previous circle
+                        // would be null if the previous circle was submitted as an LBR
                         newLBR.remove();
                     }
+
                     newLBR = googleMap.addCircle(new CircleOptions()
                             .center(new LatLng(l.latitude, l.longitude))
                             .radius(100)
@@ -99,21 +139,26 @@ public class MapsFragment extends AppFragment {
                     cancelButton.setEnabled(true);
                 });
 
-                googleMap.setOnCircleClickListener(l -> new AlertDialog.Builder(activity)
-                        .setTitle("Location based reminder")
-                        .setPositiveButton("Ok", (dialogInterface, i) -> {
-                            dialogInterface.dismiss();
-                        }).create().show());
+                googleMap.setOnCircleClickListener(l -> {
+                    Object circleTag = l.getTag();
+                    String circleTitle;
+
+                    if (circleTag == null) {
+                        circleTitle = "Null";
+                    } else {
+                        circleTitle = circleTag.toString();
+                    }
+
+                    new AlertDialog.Builder(activity)
+                            .setTitle(circleTitle)
+                            .setPositiveButton("Ok", (dialogInterface, i) -> {
+                                dialogInterface.dismiss();
+                            }).create().show();
+                });
             });
 
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                activity.requestLocationAccess();
                 return;
             }
             googleMap.setMyLocationEnabled(true);
@@ -141,18 +186,17 @@ public class MapsFragment extends AppFragment {
 
         addLBRButton = view.findViewById(R.id.map_add_reminder);
         addLBRButton.setOnClickListener(l -> {
-            Toast.makeText(getActivity(), "Added LBR", Toast.LENGTH_SHORT).show();
-            // TODO - new geofence
-
+            // convert circle added to map to LBR database object and geofence
             String lbrTitle = reminderEditText.getText().toString();
             reminderEditText.getText().clear();
 
+            // add to db
             LBRItem newLBRItem = new LBRItem(lbrTitle, newLBR.getCenter().latitude, newLBR.getCenter().longitude, newLBR.getRadius());
+            LogRoomDatabase.databaseWriteExecutor.execute(() -> dao.insertLBR(newLBRItem));
 
-            LogRoomDatabase.databaseWriteExecutor.execute(() -> {
-                dao.insertLBR(newLBRItem);
-            });
+            newLBR.setTag(newLBRItem.title);
 
+            // create geofence
             Geofence geofence = new Geofence.Builder()
                     .setRequestId(UUID.randomUUID().toString())
                     .setCircularRegion(newLBRItem.latitude, newLBRItem.longitude, 10f)
@@ -168,53 +212,52 @@ public class MapsFragment extends AppFragment {
             intent.putExtra("lbrName", newLBRItem.title);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(activity, 0, intent, PendingIntent.FLAG_MUTABLE);
             if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+                activity.requestLocationAccess();
                 return;
             }
-
             geofencingClient.addGeofences(geofencingRequest, pendingIntent)
                     .addOnSuccessListener(activity, unused -> Log.d("COMP3018", "Geofence added"))
                     .addOnFailureListener(activity, e -> Log.d("COMP3018", e.toString()));
 
+
+            // update UI
             addLBRButton.setEnabled(false);
             reminderEditText.setEnabled(false);
             cancelButton.setEnabled(false);
 
             // unset newLBR but don't remove it from the map
             newLBR = null;
+
+            Toast.makeText(getActivity(), "Added LBR", Toast.LENGTH_SHORT).show();
         });
 
         cancelButton = view.findViewById(R.id.map_cancel);
         cancelButton.setOnClickListener(v -> {
+            // get rid of current circle
             newLBR.remove();
             newLBR = null;
 
+            // update UI
             reminderEditText.getText().clear();
-
             addLBRButton.setEnabled(false);
             reminderEditText.setEnabled(false);
             cancelButton.setEnabled(false);
         });
 
-        clearLBRs = view.findViewById(R.id.map_clear_lbrs);
-        clearLBRs.setOnClickListener(v -> {
-            new AlertDialog.Builder(getActivity())
-                    .setTitle("Delete all LBRs")
-                    .setMessage("This action will delete all location-based reminders. This cannot be undone.")
-                    .setPositiveButton("Confirm", (dialogInterface, i) -> removeLBRsAndGeofences())
-                    .setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.cancel())
-                    .create().show();
-        });
+        Button clearLBRs = view.findViewById(R.id.map_clear_lbrs);
+        clearLBRs.setOnClickListener(v -> new AlertDialog.Builder(getActivity())
+                .setTitle("Delete all LBRs")
+                .setMessage("This action will delete all location-based reminders. This cannot be undone.")
+                .setPositiveButton("Confirm", (dialogInterface, i) -> removeLBRsAndGeofences())
+                .setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.cancel())
+                .create().show());
 
         reminderEditText = view.findViewById(R.id.map_edit_title);
     }
 
+    /**
+     * Method to remove all location-based reminders and Geofences.
+     */
     private void removeLBRsAndGeofences() {
         // delete all LBR rows
         LogRoomDatabase.databaseWriteExecutor.execute(() -> dao.deleteAllLBRs());
